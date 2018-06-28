bbfaae9ef15fe5e035f909d2f385c9fe52dd8741
Grace Shih <vocalise@gmail.com>
2018-06-28 07:07:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @team_name = params[:team_name]
+    @team_name = params[:name]
     @team_mates = "Coach: " + params[:coach] + "<br/>" + "Point Guard: " + params[:point_guard] + "<br/>" + "Shooting Guard: " + params[:point_guard] + "<br/>" + "Small Forward: " + params[:small_forward] + "<br />" + "Power Forward: " + params[:power_forward] + "<br/>" + "Center: " + params[:center] + "<br />"
     
     erb :team
